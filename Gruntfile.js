884b7354d9e781834a6bb7818c1f6fcb4f30af54
Jason Godson <jason_c_g@hotmail.com>
2016-08-20 08:39:38 -0600

    fixed flex for safari added grunt & autoprefixer

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    postcss: {
+        options: {
+            processors: [
+                require('autoprefixer')({
+                    browsers: ['> 1%']
+                })
+            ]
+        },
+        dist: {
+            src: 'dev/*.css',
+            dest: 'css/custom.css'
+        }
+    },
+    watch: {
+      css : {
+        files: ['dev/*'],
+        tasks: ['postcss:dist']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['postcss:dist']);
+};
